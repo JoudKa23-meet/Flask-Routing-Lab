02209d4af1e29f85d26769e1744efe95d1c5ae29
JoudKa23-meet <109899328+JoudKa23-meet@users.noreply.github.com>
2022-07-27 14:30:03 +0300

    jk

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -9,7 +9,13 @@ app = Flask(  # Create a flask app
 
 # Your code should be below
 
+@app.route("/")
+def home():
+    return render_template('home.html')
 
+@app.route("/")
+def product():
+    return render_template('product.html')
 
 
 # Your code should be above
